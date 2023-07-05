94f6a1ad62c3dfcfdcd200457b8d5be37c2fe739
Jacob Bohanon <jacobbohanon@gmail.com>
2023-07-05 12:12:59 -0400

    fix conky

--- zsh/config.d/gcloud.zsh
+++ zsh/config.d/gcloud.zsh
@@ -3,8 +3,3 @@ if [ -f "$HOME/google-cloud-sdk/path.zsh.inc" ]; then . "$HOME/google-cloud-sdk/
 
 # The next line enables shell command completion for gcloud.
 if [ -f "$HOME/google-cloud-sdk/completion.zsh.inc" ]; then . "$HOME/google-cloud-sdk/completion.zsh.inc"; fi
-
-alias startenvoydev="gcloud compute instances start bohanon-envoy-dev --zone us-central1-c --project solo-test-236622 && gcloud compute config-ssh --force-key-file-overwrite --project solo-test-236622"
-alias stopenvoydev="gcloud compute instances stop bohanon-envoy-dev --zone us-central1-c --project solo-test-236622"
-alias sshenvoydev="gcloud beta compute ssh --zone us-central1-c bohanon-envoy-dev --project solo-test-236622"
-
