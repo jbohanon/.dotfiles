1abab0d0e5c759aabb3d4315f505e8c4a6238060
Jacob Bohanon <jacob.bohanon@solo.io>
2023-12-21 12:21:55 -0500

    disable annoying autofolding

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,4 @@
 require("nvim-tree").setup()
-vim.api.nvim_set_option('foldmethod', 'expr')
-vim.api.nvim_set_option('foldexpr', 'nvim_treesitter#foldexpr()')
-vim.api.nvim_set_option('foldenable', false)
+-- vim.api.nvim_set_option('foldmethod', 'expr')
+-- vim.api.nvim_set_option('foldexpr', 'nvim_treesitter#foldexpr()')
+-- vim.api.nvim_set_option('foldenable', false)
