d3e5e47c01ec3e396dae23a8af088f36b16114c6
Jacob Bohanon <jacobbohanon@gmail.com>
2026-06-06 11:30:21 -0400

    fix the shift+enter

--- zsh/merge-main.sh
+++ zsh/merge-main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash -e
 branch=$(git status|head -1|cut -f3 -d\ )
-git checkout main
+git checkout $(git_main_branch)
 git pull
 git checkout "$branch"
-git merge main
+git merge $(git_main_branch)
